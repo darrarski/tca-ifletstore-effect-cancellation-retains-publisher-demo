590b2943af881688367636a774dc717bc14be32b
Dariusz Rybicki <darrarski@gmail.com>
2020-07-08 22:51:18 +0200

    Add App component that embeds Timer component

---
+++ Demo/App.swift
@@ -0,0 +1,59 @@
+import ComposableArchitecture
+import SwiftUI
+
+struct AppState: Equatable {
+    init(timer: TimerState? = .init()) {
+        self.timer = timer
+    }
+
+    var timer: TimerState?
+}
+
+enum AppAction: Equatable {
+    case timer(TimerAction)
+}
+
+struct AppEnvironment {
+    var mainQueue: AnySchedulerOf<DispatchQueue>
+}
+
+extension AppEnvironment {
+    var timerEnvironment: TimerEnvironment {
+        .init(mainQueue: mainQueue)
+    }
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
+    timerReducer.optional.pullback(
+        state: \.timer,
+        action: /AppAction.timer,
+        environment: \.timerEnvironment
+    )
+)
+
+struct AppView: View {
+    let store: Store<AppState, AppAction>
+
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            IfLetStore(self.store.scope(
+                state: \.timer,
+                action: AppAction.timer
+            )) { timerStore in
+                TimerView(store: timerStore)
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView(store: .init(
+            initialState: .init(),
+            reducer: .empty,
+            environment: ()
+        ))
+    }
+}
+#endif
